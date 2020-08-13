db053b946d1305d756033b7261d5ec613bd18e8a
al-aminbhuyan <alaminbhuyan321@gmail.com>
2020-08-14 00:32:03 +0600

    multiple leanerRegression

---
+++ Machine Learning/Regression_opertaion/Linear_Regression_multiple_variable.py
@@ -0,0 +1,24 @@
+#%%
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+#%%
+df = pd.read_excel("Linear_Regression_multiple_variable.xlsx")
+print(df)
+#%%
+print(df.isnull().sum())
+#%%
+# handling the null value
+fill_value = df.experience.mean()
+df.experience = df.experience.fillna(fill_value)
+#%%
+model = LinearRegression()
+model.fit(df[['speed','car_age','experience']],df.risk)
+#%%
+print(model.coef_)
+print(model.intercept_)
+#%%
+value = np.array(list(map(float,input("enter speed,car_age,experience: ").split())),ndmin=2)
+predict_value = model.predict(value)
+print("Predict risk is: ",*predict_value)
